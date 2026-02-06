e926171f365038e42abf32623614952f4a407275
abhishek3256 <trexroy16@gmail.com>
2026-02-07 01:29:48 +0530

    yes

--- server/server.js
+++ server/server.js
@@ -11,6 +11,9 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Trust Proxy for Vercel
+app.set('trust proxy', 1);
+
 // Security Middleware
 app.use(helmet());
 app.use(xss());
